a68a1b8f12ddb20de67af7cec5f9db5f669b0f25
xVerlink <justverlink@@gmail.com>
2025-04-09 16:42:47 +0300

    Homework 2.2. Context and Intent. Commit from work 09.04.2025

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,9 +12,38 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val returnBack = findViewById<ImageView>(R.id.settings_screen_return_button)
-        returnBack.setOnClickListener {
+        val returnBackButton = findViewById<ImageView>(R.id.settings_screen_return_button)
+        returnBackButton.setOnClickListener {
             finish()
+        }
+
+        val shareButton = findViewById<ImageView>(R.id.shareAppButton)
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            val sharedText = resources.getText(R.string.course_link)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, sharedText)
+            shareIntent.setType("text/plain")
+            startActivity(Intent.createChooser(shareIntent, "Куда отправить?"))
+        }
+
+        val textSupportButton = findViewById<ImageView>(R.id.textSupportButton)
+        textSupportButton.setOnClickListener {
+            val email = "justverlink@yandex.ru"
+            val uri = Uri.parse("mailto:")
+            val mailTitle = resources.getText(R.string.text_support_mail_title)
+            val mailText = resources.getText(R.string.text_support_mail_text)
+            val emailIntent = Intent(Intent.ACTION_SENDTO, uri)
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, mailTitle)
+            emailIntent.putExtra(Intent.EXTRA_TEXT, mailText)
+            startActivity(emailIntent)
+        }
+
+        val licenseAgreementButton = findViewById<ImageView>(R.id.licenseAgreementButton)
+        licenseAgreementButton.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW)
+            browserIntent.data = Uri.parse(resources.getString(R.string.practicum_offer))
+            startActivity(browserIntent)
         }
     }
 }
