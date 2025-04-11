820dc037554f692e5be620f3bafe446918ea9ce1
Nikita Saltykov <justverlink@yandex.ru>
2025-04-11 23:56:57 +0300

    Homework 2.3.2. EditText and TextWatcher. Commit from home 11.04.2025

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,6 +1,12 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,8 +17,42 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
 
         val returnBackButton = findViewById<ImageView>(R.id.search_screen_return_button)
+        val inputEditText = findViewById<EditText>(R.id.edit_text_search)
+        val clearButton = findViewById<ImageView>(R.id.clear_text_icon)
+
+
         returnBackButton.setOnClickListener {
             finish()
         }
+
+        inputEditText.setOnClickListener {
+            inputEditText.requestFocus()
+        }
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    clearButton.visibility = View.GONE
+                } else {
+                    clearButton.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        }
+        inputEditText.addTextChangedListener(textWatcher)
     }
 }
