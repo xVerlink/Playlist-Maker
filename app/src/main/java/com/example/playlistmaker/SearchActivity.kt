b5f492f0319e301edec0222b3f017d90cc159d5d
xVerlink <justverlink@@gmail.com>
2025-04-11 14:40:57 +0300

    Homework 2.3.1. EditText and TextWatcher. Commit from work 11.04.2025

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,5 +9,10 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val returnBackButton = findViewById<ImageView>(R.id.search_screen_return_button)
+        returnBackButton.setOnClickListener {
+            finish()
+        }
     }
 }
